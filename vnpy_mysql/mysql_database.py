edd676fb52f475e165288e0f8b421dfe5af3e50f
noranhe <63988546+noranhe@users.noreply.github.com>
2025-06-19 17:55:27 +0800

    [Fix] pop找不到extra的问题

--- vnpy_mysql/mysql_database.py
+++ vnpy_mysql/mysql_database.py
@@ -191,7 +191,7 @@ class MysqlDatabase(BaseDatabase):
             d["interval"] = d["interval"].value
             d.pop("gateway_name")
             d.pop("vt_symbol")
-            d.pop("extra")
+            d.pop("extra", None)
             data.append(d)
 
         # 使用upsert操作将数据更新到数据库中
@@ -249,7 +249,7 @@ class MysqlDatabase(BaseDatabase):
             d["exchange"] = d["exchange"].value
             d.pop("gateway_name")
             d.pop("vt_symbol")
-            d.pop("extra")
+            d.pop("extra", None)
             data.append(d)
 
         # 使用upsert操作将数据更新到数据库中
